67d19cb42f50e3430b650299433090a0fa466b86
Jpendy <Jpendy256@gmail.com>
2020-03-27 12:11:59 -0700

    biggest update everrr

--- app.js
+++ app.js
@@ -8,10 +8,55 @@ const verbTwoInput = document.getElementById('verbTwo');
 const verbThreeInput = document.getElementById('verbThree');
 const verbFourInput = document.getElementById('verbFour');
 
-const AdjectiveOneInput = document.getElementById('AdjectiveOne');
-const AdjectiveTwoInput = document.getElementById('AdjectiveTwo');
-const AdjectiveThreeInput = document.getElementById('AdjectiveThree');
-const AdjectiveFourInput = document.getElementById('AdjectiveFour');
+const adjectiveOneInput = document.getElementById('adjectiveOne');
+const adjectiveTwoInput = document.getElementById('adjectiveTwo');
+const adjectiveThreeInput = document.getElementById('adjectiveThree');
+const adjectiveFourInput = document.getElementById('adjectiveFour');
 
 
+const nounOneSpan = document.getElementById('nounOneSpan');
+const nounTwoSpan = document.getElementById('nounTwoSpan');
+const nounThreeSpan = document.getElementById('nounThreeSpan');
+const nounFourSpan = document.getElementById('nounFourSpan');
 
+const verbOneSpan = document.getElementById('verbOneSpan');
+const verbTwoSpan = document.getElementById('verbTwoSpan');
+const verbThreeSpan = document.getElementById('verbThreeSpan');
+const verbFourSpan = document.getElementById('verbFourSpan');
+
+const adjectiveOneSpan = document.getElementById('adjectiveOneSpan');
+const adjectiveTwoSpan = document.getElementById('adjectiveTwoSpan');
+const adjectiveThreeSpan = document.getElementById('adjectiveThreeSpan');
+const adjectiveFourSpan = document.getElementById('adjectiveFourSpan');
+
+
+const madLib = document.getElementById('madLib');
+
+const spanClass = document.getElementsByClassName('spanClass');
+
+
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', function(){
+
+    nounOneSpan.textContent = nounOneInput.value;
+    nounTwoSpan.textContent = nounTwoInput.value;
+    nounThreeSpan.textContent = nounThreeInput.value;
+    nounFourSpan.textContent = nounFourInput.value;
+
+    verbOneSpan.textContent = verbOneInput.value;
+    verbTwoSpan.textContent = verbTwoInput.value;
+    verbThreeSpan.textContent = verbThreeInput.value;
+    verbFourSpan.textContent = verbFourInput.value;
+
+    adjectiveOneSpan.textContent = adjectiveOneInput.value;
+    adjectiveTwoSpan.textContent = adjectiveTwoInput.value;
+    adjectiveThreeSpan.textContent = adjectiveThreeInput.value;
+    adjectiveFourSpan.textContent = adjectiveFourInput.value;
+
+    madLib.style.visibility = 'visible';
+    
+    spanClass.style.visibility = 'visible';
+    
+
+});
