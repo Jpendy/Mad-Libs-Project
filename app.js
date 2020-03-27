cf43ea83d596f256ac63c99a51f613bab7437796
Jpendy <Jpendy256@gmail.com>
2020-03-27 12:56:22 -0700

    latest update

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+//This is where we get all the inputs by their 'id'
 const nounOneInput = document.getElementById('nounOne');
 const nounTwoInput = document.getElementById('nounTwo');
 const nounThreeInput = document.getElementById('nounThree');
@@ -13,7 +15,7 @@ const adjectiveTwoInput = document.getElementById('adjectiveTwo');
 const adjectiveThreeInput = document.getElementById('adjectiveThree');
 const adjectiveFourInput = document.getElementById('adjectiveFour');
 
-
+//this is where we get all the spans by their 'id'
 const nounOneSpan = document.getElementById('nounOneSpan');
 const nounTwoSpan = document.getElementById('nounTwoSpan');
 const nounThreeSpan = document.getElementById('nounThreeSpan');
@@ -29,14 +31,18 @@ const adjectiveTwoSpan = document.getElementById('adjectiveTwoSpan');
 const adjectiveThreeSpan = document.getElementById('adjectiveThreeSpan');
 const adjectiveFourSpan = document.getElementById('adjectiveFourSpan');
 
-
+//this is where we get the madLib text by it's 'id'
 const madLib = document.getElementById('madLib');
 
+//this is where we get the spans all together in a class by their class name.
+//I used this to make the spans display bold in the mad lib
 const spanClass = document.getElementsByClassName('spanClass');
 
-
+//this is where I get the button by it's 'id'
 const myButton = document.getElementById('myButton');
 
+//this is where our button is connected to the function that adds all the values
+//from the inputs and places them into the spans
 myButton.addEventListener('click', function(){
 
     nounOneSpan.textContent = nounOneInput.value;
@@ -54,9 +60,9 @@ myButton.addEventListener('click', function(){
     adjectiveThreeSpan.textContent = adjectiveThreeInput.value;
     adjectiveFourSpan.textContent = adjectiveFourInput.value;
 
+    //This is the part that makes my mad lib "appear" when the button is clicked
     madLib.style.visibility = 'visible';
     
+    //this is the part that makes the bold styling appear on the spans when the button is clicked
     spanClass.style.visibility = 'visible';
-    
-
 });
