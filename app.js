17f0abfdb42a7056887aafc11e9c4f22d73f04d8
Jpendy <Jpendy256@gmail.com>
2020-03-27 09:54:12 -0700

    super update

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const nounOne = document.getElementById('nounOne');
-const nounTwo = document.getElementById('nounTwo');
-const nounThree = document.getElementById('nounThree');
-const nounFour = document.getElementById('nounFour');
+const nounOneInput = document.getElementById('nounOne');
+const nounTwoInput = document.getElementById('nounTwo');
+const nounThreeInput = document.getElementById('nounThree');
+const nounFourInput = document.getElementById('nounFour');
 
-const verbOne = document.getElementById('verbOne');
-const verbTwo = document.getElementById('verbTwo');
-const verbThree = document.getElementById('verbThree');
-const verbFour = document.getElementById('verbFour');
+const verbOneInput = document.getElementById('verbOne');
+const verbTwoInput = document.getElementById('verbTwo');
+const verbThreeInput = document.getElementById('verbThree');
+const verbFourInput = document.getElementById('verbFour');
 
-const AdjectiveOne = document.getElementById('AdjectiveOne');
-const AdjectiveTwo = document.getElementById('AdjectiveTwo');
-const AdjectiveThree = document.getElementById('AdjectiveThree');
-const AdjectiveFour = document.getElementById('AdjectiveFour');
+const AdjectiveOneInput = document.getElementById('AdjectiveOne');
+const AdjectiveTwoInput = document.getElementById('AdjectiveTwo');
+const AdjectiveThreeInput = document.getElementById('AdjectiveThree');
+const AdjectiveFourInput = document.getElementById('AdjectiveFour');
 
 
