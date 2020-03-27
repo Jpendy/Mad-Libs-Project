8f519ca8e0d0ef188ad2127b7509c96d3f929263
Jpendy <Jpendy256@gmail.com>
2020-03-26 22:52:08 -0700

    Mad libs project first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const nounOne = document.getElementById('nounOne');
+const nounTwo = document.getElementById('nounTwo');
+const nounThree = document.getElementById('nounThree');
+const nounFour = document.getElementById('nounFour');
+
+const verbOne = document.getElementById('verbOne');
+const verbTwo = document.getElementById('verbTwo');
+const verbThree = document.getElementById('verbThree');
+const verbFour = document.getElementById('verbFour');
+
+const AdjectiveOne = document.getElementById('AdjectiveOne');
+const AdjectiveTwo = document.getElementById('AdjectiveTwo');
+const AdjectiveThree = document.getElementById('AdjectiveThree');
+const AdjectiveFour = document.getElementById('AdjectiveFour');
+
+
